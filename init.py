4e77000482daa5c07e6848b6c534f16f99e6dbcc
41023108 <41023108@example.com>
2022-05-06 10:46:23 +0800

    新增onedrive內容

--- init.py
+++ init.py
@@ -10,7 +10,7 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "wcm2022 網際內容管理 KEHUEISIN網站"
+    site_title = "wcm2022 網際內容管理 KEHUEISIN個人網站"
     ip = "127.0.0.1"
     dynamic_port = 9447
     static_port = 8447
